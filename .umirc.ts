90857f27179fd133330531e89e081aa364a067e5
IrisIq <2637852337@qq.com>
2023-02-02 13:25:27 +0800

    修改config

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   npmClient: 'cnpm',
-  alias: {
-    src: '/src/',
-  },
+  history: { type: 'hash' },
+  hash: true,
+  publicPath: './',
+  routes: [
+    { path: '/', redirect: '/index' },
+    { path: '/index', component: 'index' },
+  ],
 });
